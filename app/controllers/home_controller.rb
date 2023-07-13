1910908c24ecaf2c2aa73c3d125f20d0af6168fa
falcon7.doss <falcon7.doss@gmail.com>
2023-07-13 16:11:31 +0530

    render json logics

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    render :json, only: [:id, :name, :email]
+    render json: @users, only: [:id, :name, :email]
   end
 end
